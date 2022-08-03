d5f1aebbb40e0f8ea099b9800cfa688ec74ea368
Hayden Scheiber <scheiber@chem.ubc.ca>
2022-08-03 11:56:59 -0700

    Update qme.m

--- imported_functions/qme.m
+++ imported_functions/qme.m
@@ -3,7 +3,7 @@ function qme
     switch lower(computer)
         case 'sockeye'
             [~,cmdout] = system('qstat -u $USER | tee /dev/stderr | grep haydensc | wc -l | sed -E ''s/^/Total Jobs: /''');
-        case {'cedar' 'graham'}
+        case {'cedar' 'graham' 'narval'}
             [~,cmdout] = system('squeue -u $USER -o "%10i %35j %8T %10M %10l %5D %5C %10m %11S %20a %60R " | tee /dev/stderr | wc -l | xargs printf "%d-1\n" |  bc -l | sed -E "s/^/Total Jobs: /"');
         otherwise
             [~,cmdout] = system('qstat -u $USER');
