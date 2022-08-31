e5a3b8bd19d5ae66962ef1785245962083eaa50d
Hayden Scheiber <scheiber@chem.ubc.ca>
2022-08-31 16:22:28 -0700

    Update LiX_Constraint_Fcn.m

--- Machine_Learning/LiX_Constraint_Fcn.m
+++ Machine_Learning/LiX_Constraint_Fcn.m
@@ -503,21 +503,20 @@ end
 tf = log1p(Loss) < Settings.MinSkipLoss;
 
 % Plot result to visualize
-tf_num = double(tf);
+% tf_num = double(tf);
 
 % 'r0_MM'  'r0_XX'  'epsilon_MM'  'epsilon_XX'  'gamma_MX'
 % 'sigma_MM'  'sigma_XX'  'epsilon_MM'  'epsilon_XX'
 
-
-ax1 = 'sigma_MM';
-ax2 = 'sigma_XX';
-ax3 = 'epsilon_XX';
-
-scatter3(Param.(ax1),Param.(ax2),Param.(ax3),50,tf_num,'filled')
-%set(gca, 'YScale', 'log')
-xlabel(ax1);
-ylabel(ax2);
-zlabel(ax3);
-
-clear;
+% ax1 = 'sigma_MM';
+% ax2 = 'sigma_XX';
+% ax3 = 'epsilon_XX';
+% 
+% scatter3(Param.(ax1),Param.(ax2),Param.(ax3),50,tf_num,'filled')
+% %set(gca, 'YScale', 'log')
+% xlabel(ax1);
+% ylabel(ax2);
+% zlabel(ax3);
+% 
+% clear;
 end
