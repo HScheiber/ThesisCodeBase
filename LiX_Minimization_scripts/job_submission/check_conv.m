596edf38ba00ea8e714435db932cc5197d946e54
Hayden Scheiber <scheiber@chem.ubc.ca>
2022-08-31 11:11:47 -0700

    Update check_conv.m

--- LiX_Minimization_scripts/job_submission/check_conv.m
+++ LiX_Minimization_scripts/job_submission/check_conv.m
@@ -1,10 +1,15 @@
-function stop = check_conv(~,optimValues,~,Gradient_Tol_RMS,Gradient_Tol_Max,E_Unphys)
+function stop = check_conv(~,optimValues,state,Gradient_Tol_RMS,Gradient_Tol_Max,E_Unphys)
 
-if ( rms(optimValues.gradient) < Gradient_Tol_RMS ) && max(abs(optimValues.gradient)) < Gradient_Tol_Max ...
-        || E_Unphys < optimValues.fval
-    stop = true;
-else
-    stop = false;
+switch state
+    case 'iter'
+        if ( rms(optimValues.gradient) < Gradient_Tol_RMS ) && max(abs(optimValues.gradient)) < Gradient_Tol_Max ...
+                ||  optimValues.fval < E_Unphys
+            stop = true;
+        else
+            stop = false;
+        end
+    otherwise
+        stop = false;
 end
 
 % disp(['Gradient: ' num2str(optimValues.gradient',' %.5f')])
