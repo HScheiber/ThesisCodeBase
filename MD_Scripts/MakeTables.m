86f86012ee1598d712194b3dfe58fbb57f0aa052
Hayden Scheiber <scheiber@chem.ubc.ca>
2022-09-17 20:41:10 -0700

    Incrimental save point.

--- MD_Scripts/MakeTables.m
+++ MD_Scripts/MakeTables.m
@@ -32,7 +32,7 @@ function TableFile_MX = MakeTables(Settings,varargin)
         case 'BD'
             [U_MX, U_MM, U_XX] = BD_Potential_Generator(Settings,...
                 'ReturnAsStructure','true','MDP_Minimize',MDP_Minimize);
-        case 'Mie'
+        case 'Mi'
             [U_MX, U_MM, U_XX] = Mie_Potential_Generator(Settings,...
                 'ReturnAsStructure','true','MDP_Minimize',MDP_Minimize);
     end
