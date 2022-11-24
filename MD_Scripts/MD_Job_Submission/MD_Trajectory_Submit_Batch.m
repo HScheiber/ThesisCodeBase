b7e7d405f986acb22204d2a83c388f478e09b8e9
Hayden Scheiber <scheiber@chem.ubc.ca>
2022-11-23 19:12:13 -0800

    Update MD_Trajectory_Submit_Batch.m

--- MD_Scripts/MD_Job_Submission/MD_Trajectory_Submit_Batch.m
+++ MD_Scripts/MD_Job_Submission/MD_Trajectory_Submit_Batch.m
@@ -243,6 +243,55 @@ switch lower(computer)
         Settings_array(idx).Target_T = T0; % Target temperature in kelvin. Does not apply when thermostat option 'no' is chosen
         Settings_array(idx).MDP.Initial_T = T0; % Initial termpature at which to generate velocities
 
+        %% LiI/WBK polarized Alexandria model at 909 K starting in liquid crystal structure, PR anisotropic barostat (tc = 1 ps)
+        % NPT, isotropic barostat. Anneal structure slowly to 0 kelvin.
+        Salt = 'LiI';
+        Structure = 'Liquid';
+        Theory = 'BF';
+        vdW_Type = 'WBK';
+        T0 = 909;
+
+        idx = idx+1;
+        Settings_array(idx) = Shared_Settings;
+
+        Settings_array(idx).MDP.RVDW_Cutoff = 1.0; % nm
+        Settings_array(idx).MDP.RCoulomb_Cutoff = 1.1; % nm
+        Settings_array(idx).MDP.RList_Cutoff = 1.1; % nm
+        Settings_array(idx).MDP.Disp_Correction = true; % Adds in long-range dispersion correction
+        Settings_array(idx).MinMDP.Disp_Correction = true; % Adds in long-range dispersion correction
+
+        Settings_array(idx).Theory = Theory; % Input model(s) to use: JC, JC3P, JC4P, TF, BH
+        Settings_array(idx).Salt = Salt; % Input model(s) to use: JC, JC3P, JC4P, TF, BH
+        Settings_array(idx).Structure = Structure; % Input model(s) to use: JC, JC3P, JC4P, TF, BH
+        Settings_array(idx).Model = 'Alexandria_pol'; % Name of the current model. Leave blank for the default JC/TF/BH model
+        Settings_array(idx).JobID = 'PR_1ps_anisotropic'; % An ID that is tacked onto the folder name of all current jobs
+        Settings_array(idx).N_atoms = 2000; % Minimum number of atoms to include in box or size of search box for cluster jobs. This will automatically resize as needed
+        Settings_array(idx).c_over_a = 1;
+
+        % load the model
+        Settings_array(idx) = Alexandria_Potential_Parameters(Settings_array(idx),'vdW_Type',vdW_Type);
+        Settings_array(idx).GaussianCharge = true; % Turn on Gaussian distributed charges when true
+        Settings_array(idx).Polarization = true; % Turn on polarizible Drude model when true
+
+        Settings_array(idx).PreEquilibration = 100; % ps. Relax the prepared system for this amount of time at the start with ultrafast relaxation settings.
+        Settings_array(idx).Annealing = 'no'; % Options: 'no' 'single' 'periodic'
+        Settings_array(idx).MDP.Trajectory_Time = 1.0; % ns
+
+        % Barostat Options
+        Settings_array(idx).Isotropy = 'anisotropic';
+        Settings_array(idx).Target_P = [1 1 1 1 1 1]; % Bar
+        Settings_array(idx).Barostat = 'Parrinello-Rahman'; % Options: 'no' 'Berendsen' 'Parrinello-Rahman' 'MTTK' (set NO for NVT)
+        Settings_array(idx).Time_Constant_P = 1; % 0.2 [ps] time constant for coupling P. Should be at least 20 times larger than (Nstpcouple*timestep)
+        Settings_array(idx).Nstpcouple = Get_nstcouple(Settings_array(idx).Time_Constant_P,Settings_array(idx).MDP.dt); % [ps] The frequency for coupling the pressure. The box is scaled every nstpcouple steps. 
+        Settings_array(idx).UseMoltenCompressibility = false;
+
+        % Thermostat Options
+        Settings_array(idx).Thermostat = 'v-rescale'; % Options: 'no' 'berendsen' 'nose-hoover' 'andersen' 'andersen-massive' 'nose-hoover' (set NO for NVE)
+        Settings_array(idx).Time_Constant_T = 0.2; %[ps] time constant for coupling T. Should be at least 20*Nsttcouple*timestep
+        Settings_array(idx).Nsttcouple = Get_nstcouple(Settings_array(idx).Time_Constant_T,Settings_array(idx).MDP.dt); %[ps] The frequency for coupling the temperature. 
+        Settings_array(idx).Target_T = T0; % Target temperature in kelvin. Does not apply when thermostat option 'no' is chosen
+        Settings_array(idx).MDP.Initial_T = T0; % Initial termpature at which to generate velocities
+
         %% NaCl/WBK polarized vs unpolarized Alexandria model at Tm starting in liquid vs rocksalt structure
         Salt = 'NaCl';
         Structures = {'Liquid' 'Rocksalt'};
