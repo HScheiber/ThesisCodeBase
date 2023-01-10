4e1a13e9c87cd60b1ab7eb14a5b28bdc9772b9ee
Hayden Scheiber <scheiber@chem.ubc.ca>
2023-01-09 23:29:46 -0800

    update

--- Machine_Learning/Analysis/Model_Overview.m
+++ Machine_Learning/Analysis/Model_Overview.m
@@ -2,7 +2,7 @@ clear; %#ok<*UNRCH>
 %% Data options
 Salts = {'LiF' 'LiCl' 'LiBr' 'LiI'}; %  'LiF' 'LiCl' 'LiBr' 'LiI' 'NaCl'
 Theory = 'BF';
-ModelID = 'MK';
+ModelID = 'MN';
 BestOnly = false;
 SelectOnly = [];
 Reps = [1:5];
@@ -11,7 +11,7 @@ saveloc = 'C:\Users\Hayden\Documents\Patey_Lab\Thesis_Projects\Thesis\Thesis_Dra
 DM_Multiplier = 1e5;
 
 %% Plot options
-fs = 28; % font size
+fs = 24; % font size
 markers = {'o' 's' '^' 'v' 'd' '>' '<' 'p' 'h' 'x'};
 show_as_percent_error = false; % Plot as percent error. If false, plot as the numerical error value (i.e. including units)
 include_av_line = false;
