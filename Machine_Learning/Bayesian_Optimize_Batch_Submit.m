de7d15fa01a4381483ff8c19573bf7ebb00cc0dc
Hayden Scheiber <scheiber@chem.ubc.ca>
2023-08-20 12:24:59 -0700

    Update Bayesian_Optimize_Batch_Submit.m

--- Machine_Learning/Bayesian_Optimize_Batch_Submit.m
+++ Machine_Learning/Bayesian_Optimize_Batch_Submit.m
@@ -89,7 +89,7 @@ Shared_Settings.InnerRange = true; % Sets domain of BH/TF
 Shared_Settings.GPActiveSetSize = 1000;
 
 % Shared job settings
-Shared_Settings.N_Calc = 12; % Number of chained calculations
+Shared_Settings.N_Calc = 8; % Number of chained calculations
 Shared_Settings.Hours = 6; % Max time for each job (hours)
 Shared_Settings.Mins = 0; % Max time for job (minutes)
 Shared_Settings.Nodes = 0; % Minimum number of cores to request for calculation.
